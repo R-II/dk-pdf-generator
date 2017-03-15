38a89cf5faf672185c8c3fcb536635315b8eacf6
Emili Castells <emili.castells@dinamiko.com>
2017-03-15 22:41:27 +0100

    disabled shortcode tests

--- tests/acceptance/Read_More_Cept.php
+++ tests/acceptance/Read_More_Cept.php
@@ -2,6 +2,13 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('test read more tag');
 
+$I->loginAsAdmin();
+/*
+$I->amOnPluginsPage();
+$I->activatePlugin('dk-pdf');
+$I->activatePlugin('dk-pdf-generator');
+*/
+
 $I->am( 'admin' );
 $I->wantToTest( 'see all the content in the PDF on posts with read more tag' );
 $I->amOnPage( 'wp-admin/post-new.php' );
