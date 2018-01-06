1b96db91ea2686b0e5f843d45400092a5aa7c6d5
Emili <emili.castells@dinamiko.com>
2018-01-07 00:49:25 +0100

    Compatible with dkpdf 1.9.6.

--- includes/dkpdfg-functions.php
+++ includes/dkpdfg-functions.php
@@ -84,9 +84,16 @@ function dkpdfg_output_pdf() {
 	$dkpdf_margin_header = get_option( 'dkpdf_margin_header', '15' );
 
 	// creating and setting the pdf
-	$mpdf = new mPDF( 'utf-8', $format, $dkpdf_font_size, $dkpdf_font_family,
-		$dkpdf_margin_left, $dkpdf_margin_right, $dkpdf_margin_top, $dkpdf_margin_bottom, $dkpdf_margin_header
-	);
+	$mpdf = new \Mpdf\Mpdf( [
+		'tempDir'           => realpath( __DIR__ . '/..' ) . '/tmp',
+		'default_font_size' => $dkpdf_font_size,
+		'format'            => $format,
+		'margin_left'       => $dkpdf_margin_left,
+		'margin_right'      => $dkpdf_margin_right,
+		'margin_top'        => $dkpdf_margin_top,
+		'margin_bottom'     => $dkpdf_margin_bottom,
+		'margin_header'     => $dkpdf_margin_header,
+	] );
 
 	// write cover
 	$dkpdf_show_cover = get_option( 'dkpdfg_show_cover', 'on' );
